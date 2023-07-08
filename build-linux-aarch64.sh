ba11afdaa62db15ab3ed88ecb36f89b4bc678618
tyler <tylerbrownjones@gmail.com>
2023-07-08 22:05:04 +0100

    fix linux build

--- build-linux-aarch64.sh
+++ build-linux-aarch64.sh
@@ -4,30 +4,24 @@ set -e
 
 PACKR_VERSION="runelite-1.7"
 PACKR_HASH="f61c7faeaa364b6fa91eb606ce10bd0e80f9adbce630d2bae719aef78d45da61"
+APPIMAGE_VERSION="13"
 
-SIGNING_IDENTITY="Developer ID Application"
+umask 022
 
 source .jdk-versions.sh
 
-if ! [ -f mac_aarch64_jre.tar.gz ] ; then
-    curl -Lo mac_aarch64_jre.tar.gz $MAC_AARCH64_LINK
+if ! [ -f linux_aarch64_jre.tar.gz ] ; then
+    curl -Lo linux_aarch64_jre.tar.gz $LINUX_AARCH64_LINK
 fi
 
-echo "$MAC_AARCH64_CHKSUM  mac_aarch64_jre.tar.gz" | shasum -c
+echo "$LINUX_AARCH64_CHKSUM linux_aarch64_jre.tar.gz" | sha256sum -c
 
 # packr requires a "jdk" and pulls the jre from it - so we have to place it inside
 # the jdk folder at jre/
-if ! [ -d osx-aarch64-jdk ] ; then
-    tar zxf mac_aarch64_jre.tar.gz
-    mkdir osx-aarch64-jdk
-    mv jdk-$MAC_AARCH64_VERSION-jre osx-aarch64-jdk/jre
-
-    pushd osx-aarch64-jdk/jre
-    # Move JRE out of Contents/Home/
-    mv Contents/Home/* .
-    # Remove unused leftover folders
-    rm -rf Contents
-    popd
+if ! [ -d linux-aarch64-jdk ] ; then
+    tar zxf linux_aarch64_jre.tar.gz
+    mkdir linux-aarch64-jdk
+    mv jdk-$LINUX_AARCH64_VERSION-jre linux-aarch64-jdk/jre
 fi
 
 if ! [ -f packr_${PACKR_VERSION}.jar ] ; then
@@ -35,26 +29,45 @@ if ! [ -f packr_${PACKR_VERSION}.jar ] ; then
         https://github.com/runelite/packr/releases/download/${PACKR_VERSION}/packr.jar
 fi
 
-echo "${PACKR_HASH}  packr_${PACKR_VERSION}.jar" | shasum -c
+echo "${PACKR_HASH}  packr_${PACKR_VERSION}.jar" | sha256sum -c
+
+# Note: Host umask may have checked out this directory with g/o permissions blank
+chmod -R u=rwX,go=rX appimage
+# ...ditto for the build process
+chmod 644 target/Cyphorax.jar
+
+rm -rf native-linux-aarch64
 
 java -jar packr_${PACKR_VERSION}.jar \
-	packr/macos-aarch64-config.json
+    packr/linux-aarch64-config.json
 
-cp target/filtered-resources/Info.plist native-osx-aarch64/Cyphorax.app/Contents
+pushd native-linux-aarch64/Cyphorax.AppDir
+mkdir -p jre/lib/amd64/server/
+ln -s ../../server/libjvm.so jre/lib/amd64/server/ # packr looks for libjvm at this hardcoded path
 
-echo Setting world execute permissions on Cyphorax
-pushd native-osx-aarch64/Cyphorax.app
-chmod g+x,o+x Contents/MacOS/Cyphorax
+# Symlink AppRun -> RuneLite
+ln -s Cyphorax AppRun
+
+# Ensure RuneLite is executable to all users
+chmod 755 Cyphorax
 popd
 
-codesign -f -s "${SIGNING_IDENTITY}" --entitlements osx/signing.entitlements --options runtime native-osx-aarch64/Cyphorax.app || true
-
-# create-dmg exits with an error code due to no code signing, but is still okay
-create-dmg native-osx-aarch64/Cyphorax.app native-osx-aarch64/ || true
-
-mv native-osx-aarch64/Cyphorax\ *.dmg native-osx-aarch64/Cyphorax-aarch64.dmg
-
-# Notarize app
-if xcrun notarytool submit native-osx-aarch64/Cyphorax-aarch64.dmg --wait --keychain-profile "AC_PASSWORD" ; then
-    xcrun stapler staple native-osx-aarch64/Cyphorax-aarch64.dmg
+if ! [ -f appimagetool-x86_64.AppImage ] ; then
+    curl -Lo appimagetool-x86_64.AppImage \
+        https://github.com/AppImage/AppImageKit/releases/download/$APPIMAGE_VERSION/appimagetool-x86_64.AppImage
+    chmod +x appimagetool-x86_64.AppImage
 fi
+
+echo "df3baf5ca5facbecfc2f3fa6713c29ab9cefa8fd8c1eac5d283b79cab33e4acb  appimagetool-x86_64.AppImage" | sha256sum -c
+
+if ! [ -f runtime-aarch64 ] ; then
+    curl -Lo runtime-aarch64 \
+	    https://github.com/AppImage/AppImageKit/releases/download/$APPIMAGE_VERSION/runtime-aarch64
+fi
+
+echo "d2624ce8cc2c64ef76ba986166ad67f07110cdbf85112ace4f91611bc634c96a  runtime-aarch64" | sha256sum -c
+
+ARCH=arm_aarch64 ./appimagetool-x86_64.AppImage \
+	--runtime-file runtime-aarch64  \
+	native-linux-aarch64/Cyphorax.AppDir/ \
+	native-linux-aarch64/Cyphorax-aarch64.AppImage
